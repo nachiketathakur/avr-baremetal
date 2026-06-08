70193c6daa469510b1c61a5d1bfe9bc91b61b2ff
Nachiketa Thakur <nachiketathakur8@gmail.com>
2026-06-09 00:05:35 +0530

    basic addresses of GPIOs

--- inc/my_gpio.h
+++ inc/my_gpio.h
@@ -1,4 +1,9 @@
 #ifndef _MY_GPIO_H
 #define _MY_GPIO_H
 
+/* Register definition - START*/
+#define MY_PINB  (*(volatile uint8_t *)0x23)
+#define MY_DDRB  (*(volatile uint8_t *)0x24)
+#define MY_PORTB (*(volatile uint8_t *)0x25)
+
 #endif 
