9121d90b81afc131c99f0494048e0e490082b08f
Nachiketa-rll <nachiketa@relfor.com>
2026-06-08 20:32:46 +0530

    added to ignore text file, reverted main.c file content

--- avr_main.c
+++ avr_main.c
@@ -1,16 +1,5 @@
-#include <avr/io.h>
-#include <util/delay.h>
-
-int add_numbers (int a, int b); 
 int main(void)
 {
-    add_numbers(5, 6); 
     while (1);  //this is a comment
     return 0; //this is a comment too 
-}
-
-int add_numbers (int a, int b)
-{
-    int output = a + b; 
-    return output; 
 }
